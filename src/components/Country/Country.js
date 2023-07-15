fba80fe3d1aaab5191f8516920f0f50430d94311
Rothin Roy <mr.rothinroy@gmail.com>
2023-07-15 12:39:31 +0600

    Rest countries add

---
+++ src/components/Country/Country.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Country.css';
+
+const Country = (props) => {
+    const {area, region, population, name, flags} = props.country;
+    // console.log(props.Country);
+    return (
+        <div className='country'>
+            <h2>Country: {name.common} </h2>
+            <img src={flags.png} alt="" />
+            <h3>Population: {population}</h3>
+            <h4>Region: {region}</h4>
+            <p>Area: {area}</p>
+        </div>
+    );
+};
+
+export default Country;
